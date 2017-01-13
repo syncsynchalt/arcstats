84512270d334e24c11d4a37854cf48c9e0483268
Michael Driscoll <mdriscoll@pax8.com>
2017-01-12 22:01:22 -0700

    Automatically zoom mobile to full width

--- store/lvs-export.php
+++ store/lvs-export.php
@@ -58,6 +58,7 @@ if ($type === 'pngcairo') {
 ?>
     <html>
     <head>
+    <meta name="viewport" content="width=800">
     <link rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
