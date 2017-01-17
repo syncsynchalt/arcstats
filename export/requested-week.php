5f89f763b0457a894e44420f65eac5e8728ad54c
Michael Driscoll <mdriscoll@pax8.com>
2017-01-16 20:51:40 -0700

    Everything is utf-8

--- export/requested-week.php
+++ export/requested-week.php
@@ -73,6 +73,7 @@ if ($type === 'pngcairo') {
 ?>
     <html>
     <head>
+    <meta charset="utf-8">
     <meta name="viewport" content="width=800">
     <link rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
     crossorigin="anonymous">
+<meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1,
                                maximum-scale=1, user-scalable=no">
 <meta name="format-detection" content="telephone=no">
